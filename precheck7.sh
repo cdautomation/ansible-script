1ba196ce95ce05dc79ac3bea28610030331fd6c4
harithansi <40013976+harithansi@users.noreply.github.com>
2019-05-05 12:22:29 +0530

    Add files via upload

--- precheck7.sh
+++ precheck7.sh
@@ -1,39 +1,37 @@
 #!/bin/bash
-touch /home/ansuser/prepatch.txt
-echo > /home/ansuser/prepatch.txt
-echo "----Hostname-----" >> /home/ansuser/prepatch.txt
-hostname >> /home/ansuser/prepatch.txt
-echo "----Uptime----" >> /home/ansuser/prepatch.txt
-uptime >> /home/ansuser/prepatch.txt
-echo "----Kernel Version---" >> /home/ansuser/prepatch.txt
-uname -r >> /home/ansuser/prepatch.txt
-echo "----Vmware tools Version----" >> /home/ansuser/prepatch.txt
-vmware-toolbox-cmd -v >> /home/ansuser/prepatch.txt
-echo "----LVM backup conf----- " >> /home/ansuser/prepatch.txt
-cat /etc/lvm/backup/* >> /home/ansuser/prepatch.txt
-echo "----fdisk details----" >> /home/ansuser/prepatch.txt
-fdisk -l >> /home/ansuser/prepatch.txt
-echo "----pvdisplay------" >> /home/ansuser/prepatch.txt
-pvdisplay -v >> /home/ansuser/prepatch.txt
-echo "----lvdisplay-----" >> /home/ansuser/prepatch.txt
-lvdisplay -v >> /home/ansuser/prepatch.txt
-echo "----Filesystem Details----" >> /home/ansuser/prepatch.txt
-df -h >> /home/ansuser/prepatch.txt
-echo "----Fstab details----" >> /home/ansuser/prepatch.txt
-cat /etc/fstab >> /home/ansuser/prepatch.txt
-echo "----Mtab Details----" >> /home/ansuser/prepatch.txt
-cat /etc/mtab >> /home/ansuser/prepatch.txt
-echo "----IP Address Details----" >> /home/ansuser/prepatch.txt
-ifconfig -a >> /home/ansuser/prepatch.txt
-echo "----Routing Information----" >> /home/ansuser/prepatch.txt
-netstat -nr >> /home/ansuser/prepatch.txt
-echo "----Enabled services Chkconfig------" >> /home/ansuser/prepatch.txt
-systemctl list-unit-files --type service >> /home/ansuser/prepatch.txt
-echo "----Server Details-----" >> /home/ansuser/prepatch.txt
-dmidecode -t 1 >> /home/ansuser/prepatch.txt
-echo "----Memory details----" >> /home/ansuser/prepatch.txt
-free -g >> /home/ansuser/prepatch.txt
-echo "----CPU details----" >> /home/ansuser/prepatch.txt
-lscpu >> /home/ansuser/prepatch.txt
-echo "----yum check-update details----" >> /home/ansuser/prepatch.txt
-yum check-update >> /home/ansuser/prepatch.txt
+touch /home/ansuser/configuration.txt
+echo > /home/ansuser/configuration.txt
+echo "----Hostname-----" >> /home/ansuser/configuration.txt
+hostname >> /home/ansuser/configuration.txt
+echo "----Uptime----" >> /home/ansuser/configuration.txt
+uptime >> /home/ansuser/configuration.txt
+echo "----Kernel Version---" >> /home/ansuser/configuration.txt
+uname -r >> /home/ansuser/configuration.txt
+echo "----Vmware tools Version----" >> /home/ansuser/configuration.txt
+vmware-toolbox-cmd -v >> /home/ansuser/configuration.txt
+echo "----LVM backup conf----- " >> /home/ansuser/configuration.txt
+cat /etc/lvm/backup/* >> /home/ansuser/configuration.txt
+echo "----fdisk details----" >> /home/ansuser/configuration.txt
+fdisk -l >> /home/ansuser/configuration.txt
+echo "----pvdisplay------" >> /home/ansuser/configuration.txt
+pvdisplay -v >> /home/ansuser/configuration.txt
+echo "----lvdisplay-----" >> /home/ansuser/configuration.txt
+lvdisplay -v >> /home/ansuser/configuration.txt
+echo "----Filesystem Details----" >> /home/ansuser/configuration.txt
+df -h >> /home/ansuser/configuration.txt
+echo "----Fstab details----" >> /home/ansuser/configuration.txt
+cat /etc/fstab >> /home/ansuser/configuration.txt
+echo"----Mtab Details----" >> /home/ansuser/configuration.txt
+cat /etc/mtab >> /home/ansuser/configuration.txt
+echo "----IP Address Details----" >> /home/ansuser/configuration.txt
+ifconfig -a >> /home/ansuser/configuration.txt
+echo "----Routing Information----" >> /home/ansuser/configuration.txt
+netstat -nr >> /home/ansuser/configuration.txt
+echo "----Enabled services Chkconfig------" >> /home/ansuser/configuration.txt
+systemctl list-unit-files --type service >> /home/ansuser/configuration.txt
+echo "----Server Details-----" >> /home/ansuser/configuration.txt
+dmidecode -t 1 >> /home/ansuser/configuration.txt
+echo "----Memory details----" >> /home/ansuser/configuration.txt
+free -g >> /home/ansuser/configuration.txt
+echo "----CPU details----" >> /home/ansuser/configuration.txt
+lscpu >> /home/ansuser/configuration.txt
